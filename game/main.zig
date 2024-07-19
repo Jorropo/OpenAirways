63f7d754afc6234632b04328f02d5e081ba9e604
Hamza Ali <github+hhhapz@hamza.sh>
2024-07-19 18:06:08 +0500

    setup basic game drawing planes from state

--- game/main.zig
+++ game/main.zig
@@ -1,14 +1,17 @@
 const rl = @import("raylib");
 const std = @import("std");
 const print = std.debug.print;
+
+const Thread = std.Thread;
+const Allocator = std.mem.Allocator;
 const Child = std.process.Child;
 const ArrayList = std.ArrayList;
 
-pub fn main() anyerror!void {
-    const screenWidth = 1280;
-    const screenHeight = 720;
+const screen_w = 1280;
+const screen_h = 720;
 
-    rl.initWindow(screenWidth, screenHeight, "hh-scope");
+pub fn main() anyerror!void {
+    rl.initWindow(screen_w, screen_h, "hh-scope");
     defer rl.closeWindow();
 
     var gpa = std.heap.GeneralPurposeAllocator(.{}){};
@@ -22,64 +25,153 @@ pub fn main() anyerror!void {
             .ok => {},
         }
     }
+
+    const server_args = [_][]const u8{ "./game-server", "-debug-tickmode", "slow" };
+    var proc = Child.init(&server_args, allocator);
+    proc.stdout_behavior = .Pipe;
+    proc.stderr_behavior = .Inherit;
     var state = State{};
-    try start_server(allocator, &state);
+
+    var thread = try start_server(allocator, &proc, &state);
 
     rl.setTargetFPS(60);
+
+    const plane_img = rl.loadTexture("assets/plane_1.png");
+    const plane_w: f32 = @floatFromInt(plane_img.width);
+    const plane_h: f32 = @floatFromInt(plane_img.height);
+    const plane_size = rl.Rectangle{ .x = 0, .y = 0, .width = plane_w, .height = plane_h };
+
     while (!rl.windowShouldClose()) { // Detect window close button or ESC key
         rl.beginDrawing();
+
         defer rl.endDrawing();
+        defer rl.drawFPS(8, 8); // always draw last
 
         rl.clearBackground(rl.Color.white);
-        rl.drawFPS(0, 0);
+
+        state.mu.lock();
+        for (state.planes) |plane| {
+            try plane.draw(allocator, plane_img, plane_size, true);
+        }
+        state.mu.unlock();
     }
+
+    _ = try proc.kill();
+    thread.join();
 }
 
-const XY = struct { x: i32, y: i32 };
+const XY = struct { x: f32 = 0, y: f32 = 0 };
+
+const canvas_w: f32 = 960;
+const canvas_h: f32 = 560;
 
 const Plane = struct {
-    time: u32, // last time position was materialized
-    p: XY,
-    wantHeading: u16,
-    heading: u16,
+    id: u32 = 0,
+    p: XY = .{},
+    want_heading: u16 = 0,
+    heading: u16 = 0,
+
+    fn draw(self: Plane, allocator: Allocator, img: rl.Texture, src: rl.Rectangle, draw_debug: bool) !void {
+        const origin = rl.Vector2{ .x = src.width / 2, .y = src.height / 2 };
+
+        // convert XY to coords between 0 and screen size
+        // and flip so positive y is upwards
+        const target = rl.Rectangle{
+            .x = (self.p.x + canvas_w / 2) * screen_w / canvas_w,
+            .y = (-self.p.y + canvas_h / 2) * screen_h / canvas_h,
+            .width = src.width,
+            .height = src.height,
+        };
+
+        const rot = 0;
+        rl.drawTexturePro(img, src, target, origin, rot, rl.Color.white);
+
+        if (draw_debug) {
+            const pos = try std.fmt.allocPrintZ(allocator, "id={}, pos=[{d}, {d}]", .{ self.id, self.p.x, self.p.y });
+            rl.drawText(pos, @intFromFloat(target.x + target.width / 2), @intFromFloat(target.y - target.height / 2), 16, rl.Color.red);
+            allocator.free(pos);
+        }
+    }
 };
 
-const State = struct { now: u32 = 0, planes: []Plane = &[_]Plane{}, mu: std.Thread.Mutex = .{} };
+const State = struct { now: u32 = 0, planes: []Plane = &[_]Plane{}, mu: Thread.Mutex = .{} };
 
-fn start_server(allocator: std.mem.Allocator, state: *State) !void {
-    const thread = try std.Thread.spawn(.{}, read_data, .{ allocator, state });
-    _ = thread;
+fn start_server(allocator: Allocator, proc: *Child, state: *State) !Thread {
+    const thread = try Thread.spawn(.{}, read_data, .{ allocator, proc, state });
+    return thread;
 }
 
-fn read_data(allocator: std.mem.Allocator, state: *State) !void {
-    const argv = [_][]const u8{"./server"};
-
-    var child = Child.init(&argv, allocator);
-    child.stdout_behavior = .Pipe;
-    child.stderr_behavior = .Inherit;
-
-    try child.spawn();
-    if (child.stdout == null) {
-        @panic("no stdout found");
-    }
+fn read_data(allocator: Allocator, proc: *Child, state: *State) !void {
+    try proc.spawn();
 
     var header: [8]u8 = [_]u8{0} ** 8;
-    while (child.term == null) {
-        const n = try child.stdout.?.read(&header);
-        print("read {} bytes", .{n});
-        state.mu.lock();
-        defer state.mu.unlock();
-        // const now = r_u32(header[0..4]);
+    var buffered_state = State{};
+
+    while (proc.term == null) {
+        const out = proc.stdout orelse break;
+        _ = out.read(&header) catch break;
+
         const plane_count = r_u32(header[4..8]);
-        const planes = try allocator.alloc(u8, 16 * plane_count);
-        _ = try child.stdout.?.read(planes);
+        const raw_planes = try allocator.alloc(u8, 16 * plane_count);
+        defer allocator.free(raw_planes);
+
+        _ = out.read(raw_planes) catch break;
+
+        const now = r_u32(header[0..4]);
+        buffered_state.now = now;
+
+        const old = buffered_state.planes;
+        buffered_state.planes = try allocator.alloc(Plane, plane_count);
+
+        const plane_size = 4 + // id
+            4 + // x
+            4 + // y
+            2 + // wantHeading
+            2; // heading
+
+        for (0..plane_count) |i| {
+            const offset = plane_size * i;
+
+            const id = r_u32(raw_planes[offset .. offset + 4]);
+            const x = r_f32(raw_planes[offset + 4 .. offset + 8]);
+            const y = r_f32(raw_planes[offset + 8 .. offset + 12]);
+
+            const want_heading = r_u16(raw_planes[offset + 12 .. offset + 14]);
+            const heading = r_u16(raw_planes[offset + 14 .. offset + 16]);
+
+            buffered_state.planes[i].id = id;
+            buffered_state.planes[i].p.x = x;
+            buffered_state.planes[i].p.y = y;
+            buffered_state.planes[i].want_heading = want_heading;
+            buffered_state.planes[i].heading = heading;
+        }
+
+        state.mu.lock();
+        state.*.now = buffered_state.now;
+        state.*.planes = buffered_state.planes;
+        allocator.free(old);
+        state.mu.unlock();
+
+        for (state.planes, 0..) |p, i| {
+            print("{}: plane {}: plane: {any}\n", .{ state.now, i, p });
+        }
     }
+
+    allocator.free(buffered_state.planes);
 }
 
 fn r_u16(b: []u8) u16 {
-    return b[0] | b[1] << 8;
+    return @as(u16, b[0]) | @as(u16, b[1]) << 8;
 }
 
 fn r_u32(b: []u8) u32 {
     return @as(u32, b[0]) | (@as(u32, b[1]) << 8) | (@as(u32, b[2]) << 16) | (@as(u32, b[3]) << 24);
+}
+
+fn r_f32(b: []u8) f32 {
+    return @floatFromInt(r_i32(b));
+}
+
+fn r_i32(b: []u8) i32 {
+    return @as(i32, b[0]) | (@as(i32, b[1]) << 8) | (@as(i32, b[2]) << 16) | (@as(i32, b[3]) << 24);
 }
